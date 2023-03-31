e6d4f50d08728c4abb701ade5f97d907dfb356eb
Kuba <120420285+JakUrb04@users.noreply.github.com>
2023-03-31 20:22:09 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='turbojet',
+    version='0.0.1',
+    description='Biblioteka do obliczeń związanych z silnikami lotniczymi',
+    url='https://github.com/JakUrb04/turbojet.git',
+    author='Jakub Urbaniak',
+    author_email='urbaniak627@gmail.com',
+    license='',
+    packages=['turbojet'],
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
